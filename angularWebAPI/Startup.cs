fcd189b46addd3a8de26e40ffb6b3917955b769d
Rockiez <simpi@foxmail.com>
2018-11-05 16:41:19 +0800

    add okat

---
+++ angularWebAPI/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(angularWebAPI.Startup))]
+
+namespace angularWebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var authority = "https://dev-468895.oktapreview.com/oauth2/default";
+
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                authority + "/.well-known/openid-configuration",
+                new OpenIdConnectConfigurationRetriever(),
+                new HttpDocumentRetriever());
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = "api://default",
+                    ValidIssuer = authority,
+                    IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) =>
+                    {
+                        var discoveryDocument = Task.Run(() => configurationManager.GetConfigurationAsync()).GetAwaiter().GetResult();
+                        return discoveryDocument.SigningKeys;
+                    }
+                }
+            });
+        }
+    }
+}
